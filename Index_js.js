3ce09a77bd78aa5a05212e9a5d4d1aa2c3d796b5
Feleks_Webs <49903400+FeleksWebs@users.noreply.github.com>
2019-09-22 19:37:04 +0100

    Add files via upload

---
+++ Index_js.js
@@ -0,0 +1,47 @@
+var DisplayTime = 3000;
+var SwitchToRight = DisplayTime+1000;
+
+
+
+
+
+function startAnim(){
+  setTimeout(function(){
+      
+      function FirstAnim(){
+      setTimeout (function(){$('.First-Image').animate({width: '0px'},'slow','linear')},DisplayTime);
+      setTimeout (function(){$('.First-Image').css('right','0')},SwitchToRight);
+
+      setTimeout (function(){$('.Second-Image').animate({width: '100%'},'slow','linear')},DisplayTime-1);
+      setTimeout (function(){$('.Second-Image').css('left','0')},SwitchToRight);  
+
+      setTimeout(function(){SecondAnim()},DisplayTime)
+    }
+
+      function SecondAnim(){    
+      setTimeout (function(){$('.Second-Image').animate({width: '0px'},'slow','linear')},DisplayTime);
+      setTimeout (function(){$('.Second-Image').css('left','')},SwitchToRight); 
+
+      setTimeout (function(){$('.Third-Image').animate({width: '100%'},'slow','linear')},DisplayTime-1);
+      setTimeout (function(){$('.Third-Image').css('left','0')},SwitchToRight);  
+
+      setTimeout(function(){ThirdAnim()},SwitchToRight)
+    }
+
+    function ThirdAnim(){
+      setTimeout (function(){$('.Third-Image').animate({width: '0px'},'slow','linear')},DisplayTime-1);
+      setTimeout (function(){$('.Third-Image').css('left','')},SwitchToRight);  
+
+      setTimeout (function(){$('.First-Image').animate({width: '100%'},'slow','linear')},DisplayTime);
+      setTimeout (function(){$('.First-Image').css('right','')},SwitchToRight);
+
+      setTimeout(function(){FirstAnim()},SwitchToRight)
+    }
+
+  FirstAnim()
+
+  
+  },1000)
+}
+
+startAnim()
