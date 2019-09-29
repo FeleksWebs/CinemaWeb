06c53552099008f1b55e2b47da520f83e64610ee
Feleks_Webs <49903400+FeleksWebs@users.noreply.github.com>
2019-09-29 15:56:13 +0100

    Updated Files

--- Index_js.js
+++ Index_js.js
@@ -1,10 +1,6 @@
 var DisplayTime = 3000;
 var SwitchToRight = DisplayTime+1000;
 
-
-
-
-
 function startAnim(){
   setTimeout(function(){
       
@@ -12,27 +8,25 @@ function startAnim(){
       setTimeout (function(){$('.First-Image').animate({width: '0px'},'slow','linear')},DisplayTime);
       setTimeout (function(){$('.First-Image').css('right','0')},SwitchToRight);
 
-      setTimeout (function(){$('.Second-Image').animate({width: '100%'},'slow','linear')},DisplayTime-1);
+      setTimeout (function(){$('.Second-Image').animate({width: '100%'},'slow','linear')},DisplayTime-5);
       setTimeout (function(){$('.Second-Image').css('left','0')},SwitchToRight);  
 
       setTimeout(function(){SecondAnim()},DisplayTime)
     }
-
       function SecondAnim(){    
       setTimeout (function(){$('.Second-Image').animate({width: '0px'},'slow','linear')},DisplayTime);
       setTimeout (function(){$('.Second-Image').css('left','')},SwitchToRight); 
 
-      setTimeout (function(){$('.Third-Image').animate({width: '100%'},'slow','linear')},DisplayTime-1);
+      setTimeout (function(){$('.Third-Image').animate({width: '100%'},'slow','linear')},DisplayTime-5);
       setTimeout (function(){$('.Third-Image').css('left','0')},SwitchToRight);  
 
       setTimeout(function(){ThirdAnim()},SwitchToRight)
     }
-
     function ThirdAnim(){
-      setTimeout (function(){$('.Third-Image').animate({width: '0px'},'slow','linear')},DisplayTime-1);
+      setTimeout (function(){$('.Third-Image').animate({width: '0px'},'slow','linear')},DisplayTime);
       setTimeout (function(){$('.Third-Image').css('left','')},SwitchToRight);  
 
-      setTimeout (function(){$('.First-Image').animate({width: '100%'},'slow','linear')},DisplayTime);
+      setTimeout (function(){$('.First-Image').animate({width: '100%'},'slow','linear')},DisplayTime-5);
       setTimeout (function(){$('.First-Image').css('right','')},SwitchToRight);
 
       setTimeout(function(){FirstAnim()},SwitchToRight)
